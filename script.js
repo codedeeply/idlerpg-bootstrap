66a037deecceb43a5c9b5d85bde0ef46bce2a87e
Sam Thacker <sammyt523@gmail.com>
2018-05-15 15:08:57 -0700

    Updated targeting for changed classing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.pathname;
-	$('nav ul li a').click(function(){ $('li a').removeClass("active"); $(this).addClass("active"); });
+	$('ul li a.nav-item').click(function(){ $('li a').removeClass("active"); $(this).addClass("active"); });
 })
