66cd7a0581da1555d409274524455960d077bbdf
Sam Thacker <sammyt523@gmail.com>
2018-05-15 15:17:17 -0700

    Updated pathname detection script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// get current URL path and assign 'active' class
-	var pathname = window.location.pathname;
+	var pathname = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
 	console.log(pathname);
 	$('ul li a.nav-item').click(function(){ $('li a.nav-item').removeClass("active"); $(this).addClass("active"); });
 })
