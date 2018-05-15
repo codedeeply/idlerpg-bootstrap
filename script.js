459c8b4d115a69fb4ea69a921527a851d2f42754
Sam Thacker <sammyt523@gmail.com>
2018-05-15 15:19:26 -0700

    Specified link class detection

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
 	console.log(pathname);
-	$('ul li a.nav-item').click(function(){ $('li a.nav-item').removeClass("active"); $(this).addClass("active"); });
+	$('ul.nav.navbar-item li.nav-item a.nav-link').click(function(){ $('li.nav-item a.nav-link').removeClass("active"); $(this).addClass("active"); });
 })
