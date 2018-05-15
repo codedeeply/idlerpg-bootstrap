bd5524d50e87abf0f69b3e6e75b7232504818191
Sam Thacker <sammyt523@gmail.com>
2018-05-15 15:12:31 -0700

    Debug log pathname

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.pathname;
+	console.log(pathname);
 	$('ul li a.nav-item').click(function(){ $('li a.nav-item').removeClass("active"); $(this).addClass("active"); });
 })
