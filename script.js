1330d476bcb61376ee2c75c83c7fb6e57e0f5379
Sam Thacker <sammyt523@gmail.com>
2018-05-15 15:09:45 -0700

    Added additional nav classing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.pathname;
-	$('ul li a.nav-item').click(function(){ $('li a').removeClass("active"); $(this).addClass("active"); });
+	$('ul li a.nav-item').click(function(){ $('li a.nav-item').removeClass("active"); $(this).addClass("active"); });
 })
