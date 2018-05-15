4c554fceb7815ddc8fe531c530012b3f2bd862dc
Sam Thacker <sammyt523@gmail.com>
2018-05-14 19:05:57 -0700

    Bootstrapped footer styling

--- footer.php
+++ footer.php
@@ -1,32 +1,31 @@
     </div>
-    <div class="footer">
-        <p class="small">
-            Questions? Comments? Suggestions? Bugs? Naked pics?
-            <!--<a href="contact.php">Send an email</a> or --><?php print $admin_nick?>@IRC.
+    <footer class="footer">
+        Questions? Comments? Suggestions? Bugs? Naked pics?
+        <!--<a href="contact.php">Send an email</a> or --><?php print $admin_nick?>@IRC.
 <?php
-            $hits = file("hits.db");
-            $fp = fopen("hits.db", "w");
-            $thispage = explode("/",$_SERVER['PHP_SELF']);
-            $thispage = array_pop($thispage);
-            if ($fp == false) {
-                echo "Error: could not open file hits.db.";
+        $hits = file("hits.db");
+        $fp = fopen("hits.db", "w");
+        $thispage = explode("/",$_SERVER['PHP_SELF']);
+        $thispage = array_pop($thispage);
+        if ($fp == false) {
+            echo "Error: could not open file hits.db.";
+        }
+        foreach ($hits as $line) {
+            list($page,$numhits,$date) = explode("\t",trim($line));
+            if ($page == $thispage) {
+                ++$numhits;
+                echo "            $numhits hits since $date";
+                $found = 1;
             }
-            foreach ($hits as $line) {
-                list($page,$numhits,$date) = explode("\t",trim($line));
-                if ($page == $thispage) {
-                    ++$numhits;
-                    echo "            $numhits hits since $date";
-                    $found = 1;
-                }
-                if ($fp) {
-                    fwrite($fp,"$page\t$numhits\t$date\n");
-                }
+            if ($fp) {
+                fwrite($fp,"$page\t$numhits\t$date\n");
             }
-            if (!$found && $fp) {
-                echo "            1 hit since ".date("M j, Y",time());
-                fwrite($fp,$thispage."\t1\t".date("M j, Y",time())."\n");
-            }
-            fclose($fp);
+        }
+        if (!$found && $fp) {
+            echo "            1 hit since ".date("M j, Y",time());
+            fwrite($fp,$thispage."\t1\t".date("M j, Y",time())."\n");
+        }
+        fclose($fp);
 ?>
 
         </p>
