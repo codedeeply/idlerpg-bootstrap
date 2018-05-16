433d23da873d8ffb4b905d4e8196c73806e97def
Sam Thacker <sammyt523@gmail.com>
2018-05-15 17:23:49 -0700

    Swap nav classing position

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 
         <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                  <li class="nav-item" <?php echo ($current_url == "index.php") ? $active : ''?>>
+                  <li <?php echo ($current_url == "index.php") ? $active : ''?> class="nav-item">
                     <a class="nav-link" href="index.php">Game Info</a>
                   </li>
                   <li class="nav-item">
